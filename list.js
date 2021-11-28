422f18f7ec3a1d9b790385a561557efd31ad5c97
AthulMckell <51241239+AthulMckell@users.noreply.github.com>
2021-11-28 22:50:59 +0530

    changes

--- list.js
+++ list.js
@@ -1,9 +1,10 @@
+var getChk=new Array();
 function alist(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
         if(this.readyState==4&&this.status==200){
             var response = JSON.parse(this.responseText);
-            // var count = document.getElementById('input[type="checkbox"]:checked').length;
+          
             var output = "";
             for(var i=0;i<response.length;i++){
                
@@ -11,16 +12,31 @@ function alist(){
                     output +="<h4>" + "<input type='checkbox' id='chkbox' checked disabled>"  + " ." + response[i].title + "</h4>" + "<br>";
                 }
                 else{
-                    output += "<h4>" + "<input type='checkbox' id='chkbox2'>" + " ." +response[i].title + "</h4>" + "<br>";
-                   
-                     
-                }
+                    output += "<h4>" + "<input type='checkbox' id='chkbox2' name='chkreal' onclick='countCheckboxes();' value='che'>" + " ." +response[i].title + "</h4>" + "<br>";
+                        
+                        
+                          
+                    }                  
             }
+        }
             document.getElementById("list").innerHTML=output;
-        
-           }
-        
+          
     }
     xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
     xhttp.send();
-    }
+}
+
+
+// funtioncount
+function countCheckboxes(){
+//  
+ var a = document.forms["list"];
+ var x =a.querySelectorAll('input[type="checkbox"]:checked');
+ if(x.length==95)
+{
+    alert("WOW!!!!! you've completed 5 Tasks");
+}
+
+// 
+            }
+// funtionend
